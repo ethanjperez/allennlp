5731f417c675c9c3c1e8fa4d512ea9fd25f362ba
ethanjperez <ethanperez18@gmail.com>
2018-10-15 10:16:58 -0400

    Debugging

--- allennlp/commands/evaluate.py
+++ allennlp/commands/evaluate.py
@@ -122,7 +122,7 @@ def evaluate(model: Model,
                 brute_force_metrics = []
                 import ipdb; ipdb.set_trace()
                 batch_passage_tokens = batch['passage']['tokens'].clone()
-                batch_passage_token_characters = batch['passage']['tokens'].clone()
+                batch_passage_token_characters = batch['passage']['token_characters'].clone()
                 for a_idx in range(num_sents):
                     for b_idx in range(num_sents):
                         ab_sent_idxs = torch.stack([torch.LongTensor((a_idx, b_idx)) for i in range(num_sents.size(0))])
@@ -130,8 +130,8 @@ def evaluate(model: Model,
                         if bool((sent_masks == 2).any()):
                             sent_masks /= 2  # If b decides to not reveal (i.e., to reveal same part as a), then make sure input isn't magnified
                         pad_masks = (batch['passage']['tokens'] != 0).long()
-                        # batch['passage']['tokens'] = ((batch_passage_tokens * sent_masks) + ((1 - sent_masks) * period_token_no)) * pad_masks
-                        # batch['passage']['token_characters'] = ((batch_passage_token_characters * sent_masks.unsqueeze(-1)) + ((1 - sent_masks.unsqueeze(-1)) * period_token_no)) * pad_masks.unsqueeze(-1)
+                        batch['passage']['tokens'] = ((batch_passage_tokens * sent_masks) + ((1 - sent_masks) * period_token_no)) * pad_masks
+                        batch['passage']['token_characters'] = ((batch_passage_token_characters * sent_masks.unsqueeze(-1)) + ((1 - sent_masks.unsqueeze(-1)) * period_token_no)) * pad_masks.unsqueeze(-1)
                         batch = util.move_to_device(batch, cuda_device)
                         model(**batch)
                         brute_force_metrics.append(model.get_metrics())
