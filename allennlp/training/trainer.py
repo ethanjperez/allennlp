77ba069e23e88b71a33666b51bd128d7993a198e
ethanjperez <ethanperez18@gmail.com>
2018-10-20 08:44:48 -0400

    Fixing PG with baseline

--- allennlp/training/trainer.py
+++ allennlp/training/trainer.py
@@ -494,9 +494,13 @@ class Trainer(Registrable):
                 a_turn = (turn % 2) == 0
                 grad_dir = -1 if a_turn else 1
                 baseline = values[turn].to(j_correct)  # Rougher baseline: j_correct.mean()
-                advantage = j_correct - baseline.detach()
-                output_dict['loss'] += grad_dir * (torch.log(sent_action_probs[turn]) * advantage).sum()  # Actor loss
-                output_dict['loss'] += ((j_correct - baseline) ** 2).sum()  # Critic loss
+                output_dict['loss'] += grad_dir * (torch.log(sent_action_probs[turn]) * (j_correct - baseline.detach())).mean()  # Policy loss
+                value_loss = 0.5 * ((j_correct - baseline) ** 2).mean()  # Value loss
+                output_dict['loss'] += value_loss
+                if (self._batch_num_total % 50) == 0:
+                    print('This batch:')
+                    print(' * V(s)      ~=', baseline.mean(), 'for a_turn =', a_turn)
+                    print(' * V(s) Loss ~=', value_loss, 'for a_turn =', a_turn)
 
         try:
             loss = output_dict["loss"]
