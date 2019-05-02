c4cd9d1ecf6bae2ce999fb00eac7b2a22d34198b
ethanjperez <ethanperez18@gmail.com>
2019-05-01 20:15:05 -0400

    Adding SL Debate logs

--- training_config/commands_launch.sh
+++ training_config/commands_launch.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 COMMANDS=(
-"allennlp train training_config/bert_mc_pq2a.race.lr=5e-6.bsz=4.jsonnet -s tmp/race.bert_mc_pq2a.bsz=32.lr=5e-6.a=8.f -d f -a 8 -f"
-"allennlp train training_config/bert_mc_pq2a.race.lr=5e-6.bsz=4.jsonnet -s tmp/race.bert_mc_pq2a.bsz=16.lr=5e-6.a=4.f -d f -a 4 -f"
-"allennlp train training_config/bert_mc_pq2a.race.lr=1e-5.bsz=4.jsonnet -s tmp/race.bert_mc_pq2a.bsz=16.lr=1e-5.a=4.f -d f -a 4 -f"
+"allennlp train training_config/race.best.jsonnet -s tmp/race.best.f -e -r -d Ⅰ -c concat -p tmp/race.best.f/oracle_outputs.c=concat.d=Ⅰ.test.pkl -o \"{'train_data_path': 'allennlp/tests/fixtures/data/race_raw/train', 'validation_data_path': 'datasets/race_raw/test'}\" 2>&1 | tee tmp/race.best.f/d=Ⅰ.c=concat.test.txt"
+"allennlp train training_config/race.best.jsonnet -s tmp/race.best.f -e -r -d Ⅱ -c concat -p tmp/race.best.f/oracle_outputs.c=concat.d=Ⅱ.test.pkl -o \"{'train_data_path': 'allennlp/tests/fixtures/data/race_raw/train', 'validation_data_path': 'datasets/race_raw/test'}\" 2>&1 | tee tmp/race.best.f/d=Ⅱ.c=concat.test.txt"
+"allennlp train training_config/race.best.jsonnet -s tmp/race.best.f -e -r -d Ⅲ -c concat -p tmp/race.best.f/oracle_outputs.c=concat.d=Ⅲ.test.pkl -o \"{'train_data_path': 'allennlp/tests/fixtures/data/race_raw/train', 'validation_data_path': 'datasets/race_raw/test'}\" 2>&1 | tee tmp/race.best.f/d=Ⅲ.c=concat.test.txt"
+"allennlp train training_config/race.best.jsonnet -s tmp/race.best.f -e -r -d Ⅳ -c concat -p tmp/race.best.f/oracle_outputs.c=concat.d=Ⅳ.test.pkl -o \"{'train_data_path': 'allennlp/tests/fixtures/data/race_raw/train', 'validation_data_path': 'datasets/race_raw/test'}\" 2>&1 | tee tmp/race.best.f/d=Ⅳ.c=concat.test.txt"
 )
 
 for COMMAND in "${COMMANDS[@]}"; do
     export COMMAND_ARRAY=($COMMAND)
     export SERIALIZATION_DIR="${COMMAND_ARRAY[4]}"
-#    if test -e $SERIALIZATION_DIR; then echo -e "\n${PURPLE}NOTICE: Directory already exists.\n"; else mkdir -p $SERIALIZATION_DIR; fi
+    if test -e $SERIALIZATION_DIR; then echo -e "\n${PURPLE}NOTICE: Directory already exists.\n"; else mkdir -p $SERIALIZATION_DIR; fi
     sbatch --job-name $SERIALIZATION_DIR --mem=20000 -t 1-23:58 --gres=gpu:1080ti:1 --open-mode append --requeue --wrap "$COMMAND"
     echo -e "\n${CYAN}${SERIALIZATION_DIR}/train.log\n"
 done
