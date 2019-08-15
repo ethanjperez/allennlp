f2355a89d81419438b12706dc2204b7f8f5f5c24
Siddharth Karamcheti <siddk@fb.com>
2019-08-15 15:52:05 -0400

    Clean up repo

--- dream_experiments/results/graphs-5-15/report.md
+++
@@ -1,9 +0,0 @@
-Mode - BERT Large
-------------------------------
-	LR: 1e-5	BSZ: 8	Best Epoch: 7	Best Valid Acc: 0.26867
-	LR: 2e-5	BSZ: 8	Best Epoch: 4	Best Valid Acc: 0.25169
-	LR: 1e-5	BSZ: 12	Best Epoch: 4	Best Valid Acc: 0.28566
-	LR: 5e-6	BSZ: 12	Best Epoch: 7	Best Valid Acc: 0.71168*
-	LR: 3e-5	BSZ: 16	Best Epoch: 2	Best Valid Acc: 0.25926
-	LR: 5e-6	BSZ: 16	Best Epoch: 7	Best Valid Acc: 0.70411
-	LR: 2e-5	BSZ: 32	Best Epoch: 2	Best Valid Acc: 0.25742
--- tf_idf/ans_agnostic.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python tf_idf/run_ans_agnostic.py -m q-only -t datasets/race_raw/train -v datasets/race_raw/test; python tf_idf/run_ans_agnostic.py -m n-sents -t datasets/race_raw/train -v datasets/race_raw/test; python fasttext/run_ans_agnostic.py -v datasets/race_raw/test; python tf_idf/run_ans_agnostic.py -m q-only -s dream -t datasets/dream/train.json -v datasets/dream/test.json; python tf_idf/run_ans_agnostic.py -m n-sents -s dream -t datasets/dream/train.json -v datasets/dream/test.json; python fasttext/run_ans_agnostic.py -d dream -v datasets/dream/test.json
